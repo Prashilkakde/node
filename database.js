d9ce64520c3f8c11de4e452139613cdb4ce8da02
Prashilkakde <kakde.prashil@gmail.com>
2025-03-23 21:27:35 +0530

    First commit

--- database.js
+++ database.js
@@ -1,2 +1,50 @@
-const URI =
+const { MongoClient } = require("mongodb");
+
+const url =
   "mongodb+srv://prashilkakde29:BTtINYEjKGy2MMyi@cluster0.vs1a9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(url);
+
+const dbName = "HelloWorld";
+
+async function main() {
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  const collection = db.collection("User");
+
+  const data = {
+    firstName: "Virat",
+    lastName: "Kohli",
+    city: "Delhi",
+    phoneNumber: "IDN",
+  };
+
+  // Add / Insert
+
+  //   const insertResult = await collection.insertMany([data]);
+  //   console.log("Inserted documents =>", insertResult);
+
+  // Read or Find
+
+  const findResult = await collection.find({}).toArray();
+  console.log("Found documents =>", findResult);
+
+  // Update
+
+  const updateResult = await collection.updateOne(
+    {
+      /* filter criteria here */
+    }, // Specify which document to update
+    { $set: { firstName: "Prash" } } // Update operation
+  );
+
+  console.log("Updated documents =>", updateResult);
+
+  return "Done.";
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
